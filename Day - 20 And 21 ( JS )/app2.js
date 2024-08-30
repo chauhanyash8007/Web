c829fd503935f6e34bd609dd6903d361984e33f6
Yash Chauhan <chauhanyash8007@gamil.com>
2024-08-30 23:46:13 +0530

    added

--- Day - 20 And 21 ( JS )/app2.js
+++ Day - 20 And 21 ( JS )/app2.js
@@ -93,11 +93,11 @@ console.log(result);
 
 let numS1 = [2, 3, 4, 5, 3, 4, 7, 8, 1, 2];
 
-let result1 = numS.reduce((max, el) => {
-  if (max > el) {
+let result1 = numS.reduce((min, el) => {
+  if (min > el) {
     return el;
   } else {
-    return max;
+    return min;
   }
 });
 
@@ -105,3 +105,124 @@ console.log(result1);
 
 let numbers = [90, 40, 100].every((el) => el % 10 == 0);
 console.log(numbers);
+
+// default parameters
+
+function sum(a, b = 2) {
+  console.log(a + b);
+}
+sum(1);
+
+// spread
+
+console.log(..."Yash");
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+console.log(...arr1);
+console.log(Math.min(...arr1));
+
+//
+
+let arr4 = [1, 2, 3, 4, 5];
+let newArr = [...arr4];
+console.log(newArr);
+console.log(...newArr);
+
+let str = [..."Yash"];
+console.log(str);
+
+// arrays literal
+let odd = [1, 3, 5, 7, 9];
+let eveN = [2, 4, 6, 8, 10];
+
+let ans = [...odd, ...eveN];
+console.log(ans);
+
+// objects literal
+
+let data = {
+  Name: "Yash",
+  age: 21,
+};
+
+let dataCopy = { ...data, weight: 60 };
+console.log(dataCopy);
+
+// extra
+
+let arr5 = [1, 2, 3, 4, 5];
+let obj1 = { ...arr5 };
+console.log(obj1);
+let obj2 = { ..."hello" };
+console.log(obj2);
+
+// Rest
+
+function sum(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log("you gave us", args[i]);
+  }
+}
+
+sum(1, 2, 3, 4);
+
+function min() {
+  console.log(arguments);
+  console.log(arguments.length);
+  // arguments.push(1); // here argument is collection not an array so can not use array functions here
+}
+min(1, 2, 3, 4);
+
+// destructuring
+
+let names = ["tony", "bruce", "peter", "steve"];
+
+let [winner, ...others] = names;
+console.log(winner);
+console.log(others);
+
+// destructuring (object)
+
+const student1 = {
+  name: "Yash",
+  age: 21,
+  class: 10,
+  subject: ["eng", "math", "phy"],
+  username: "Yash123",
+  password: 1234,
+};
+
+let { username: user, password: pass } = student1;
+console.log(user);
+console.log(pass);
+
+//
+
+let arr2 = [1, 2, 3, 4, 5];
+let square = arr2.map((el) => {
+  return el * el;
+});
+
+console.log(square);
+
+let sum10 = square.reduce((sum, el) => {
+  return sum + el;
+});
+
+console.log(sum10);
+
+let avg = sum10 / arr2.length;
+console.log(avg);
+
+console.log(arr2.map((el) => el + 5));
+
+let str4 = ["Yash", "Rishi", "Priyanshu", "Karan"];
+console.log(str4.map((el) => el.toUpperCase()));
+
+let doubleAndReturnArgs = (arr, ...args) => [arr, args.map((el) => el * 2)];
+
+console.log(doubleAndReturnArgs([1, 2, 3], 4, 5, 6));
+
+let mergeObject = (obj1, obj2) => ({ ...obj1, ...obj2 });
+
+console.log(mergeObject({ a: 1, b: 2 }, { c: 3, d: 4 }));
