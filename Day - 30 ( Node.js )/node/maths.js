f227b636986f3f81c479cd3049ae78b04764b4a0
Yash Chauhan <chauhanyash8007@gamil.com>
2024-10-06 13:02:08 +0530

    added node

---
+++ Day - 30 ( Node.js )/node/maths.js
@@ -0,0 +1,30 @@
+// module.exports.sum = (a, b) => a + b;
+// module.exports.mul = (a, b) => a * b;
+// module.exports.g = 9.8;
+// module.exports.pi = 3.14;
+
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const g = 9.8;
+export const pi = 3.14;
+
+// const sum = (a, b) => a + b;
+// const mul = (a, b) => a * b;
+// const g = 9.8;
+// const pi = 3.14;
+
+// let obj = {
+//   sum: sum,
+//   mul: mul,
+//   g: g,
+//   PI: pi,
+// };
+
+// module.exports = obj;
+
+// module.exports = {
+//   sum: sum,
+//   mul: mul,
+//   g: g,
+//   PI: pi,
+// };
