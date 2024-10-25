9769a3c2034debf2de7c4119f576ab9c65acbea8
Yash Chauhan <chauhanyash8007@gamil.com>
2024-10-25 17:56:23 +0530

    removed

--- Day - 30 ( Node.js )/Figlet/index.js
+++
@@ -1,9 +0,0 @@
-const figlet = require("Figlet");
-figlet("Hello World!!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
